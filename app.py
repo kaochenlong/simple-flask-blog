5366a08dfac09ae02ae21648d7c3ffc06df81463
Eddie Kao <eddie@5xcampus.com>
2024-07-01 18:14:45 +0800

    add create function

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/posts")
 def index():
     return render_template("posts/index.html.jinja")
 
+
 @app.route("/posts/new")
 def new():
     return render_template("posts/new.html.jinja")
+
+
+@app.route("/posts/create", methods=["POST"])
+def create():
+    # 寫入資料庫，待會做...
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(port=9527, debug=True)
