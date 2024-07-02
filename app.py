9b2bdb1b6c1fade4f3fa65f87955128977713fe7
Eddie Kao <eddie@5xcampus.com>
2024-07-02 17:47:30 +0800

    add migration

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, url_for
 import os
 from config.settings import db
+from flask_migrate import Migrate
+import models
 
 app = Flask(__name__)
 
@@ -25,6 +27,7 @@ def create():
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{ROOT_PATH}/db/blog.sqlite"
 db.init_app(app)
+Migrate(app, db)
 
 if __name__ == "__main__":
     app.run(port=9527, debug=True)
