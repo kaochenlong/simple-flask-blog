df062d8bd652e71887083d7c09819606d4fbbbcc
Eddie Kao <eddie@5xcampus.com>
2024-07-02 16:03:24 +0800

    add Post model

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 import os
+from config.settings import db
 
 app = Flask(__name__)
 
@@ -24,12 +24,7 @@ def create():
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{ROOT_PATH}/db/blog.sqlite"
-
-db = SQLAlchemy()
 db.init_app(app)
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
     app.run(port=9527, debug=True)
