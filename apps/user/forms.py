107ed28bdf5282b2834d155ad9b799e043d24ec8
Eddie Kao <eddie@5xcampus.com>
2024-07-08 20:57:08 +0800

    add form validations

---
+++ apps/user/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+from models.user import User
+
+STYLES = "text-xl w-full md:w-1/2 px-3 py-2 border border-gray-900 rounded-sm"
+
+
+def unique_username(form, field):
+    if User.query.filter_by(username=field.data).first():
+        raise ValidationError("該帳號已被申請")
+
+
+class UserRegisterForm(Form):
+    username = StringField(
+        "帳號",
+        validators=[InputRequired(), Length(min=4), unique_username],
+        render_kw={"placeholder": "使用者帳號", "class": STYLES},
+    )
+    password = PasswordField(
+        "密碼",
+        validators=[InputRequired()],
+        render_kw={"placeholder": "請填寫登入密碼", "class": STYLES},
+    )
+    password_confirm = PasswordField(
+        "密碼確認",
+        validators=[InputRequired(), EqualTo("password", message="密碼確認不符")],
+        render_kw={"placeholder": "再次確認密碼", "class": STYLES},
+    )
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,14 +1,26 @@
 from flask import Blueprint, render_template, request, redirect, flash, url_for
-from models import User
+from .forms import UserRegisterForm
+from models.user import User
+from config.settings import db
 
 user_bp = Blueprint("user", __name__)
 
 
 @user_bp.route("/sign_up")
 def new():
-    return render_template("users/new.html.jinja")
+    form = UserRegisterForm()
+    return render_template("users/new.html.jinja", form=form)
 
 
 @user_bp.route("/create", methods=["POST"])
 def create():
-    pass
+    form = UserRegisterForm(request.form)
+
+    if form.validate():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("註冊成功！")
+        return redirect(url_for("root"))
+
+    return render_template("users/new.html.jinja", form=form)
